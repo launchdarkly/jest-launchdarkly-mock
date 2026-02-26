f9348e89a23d3bb2dfb95350140f0ad24d1e345d
joker23 <2494686+joker23@users.noreply.github.com>
2026-02-26 10:42:18 -0600

    refactor: examples and build (#98)
    This PR will start the refactoring of this repo to be a bit more maintainable. A few changes:
    1. Fixed tsconfig to use more modern stuff
    2. Updated some yarn packages (will still need to think about why we are trying to do a no-install-module repo)
    3. Redid the example so it show cases the module (and not the react sdk?)
    4. updated some build param/test to be more in line with their purpose

--- example/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  setupFiles: ['jest-launchdarkly-mock'],
-  testEnvironment: 'jsdom',
-  testPathIgnorePatterns: ['tsoutput'],
-  verbose: true,
-}
---
+++ example/src/Button.test.tsx
@@ -0,0 +1,94 @@
+/** @jest-environment jsdom */
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+
+// This should still be a valid test since this should just be a 'side-effect import'
+// when used like this.
+import { mockFlags, ldClientMock, resetLDMocks } from '../../dist/index.js'
+
+import Button from './Button'
+
+describe('feature flag controls rendering', () => {
+  beforeEach(() => {
+    resetLDMocks()
+  })
+
+  it('renders disabled when no flags are set', () => {
+    const { getByText } = render(<Button />)
+    expect(getByText('button disabled')).toBeTruthy()
+  })
+
+  it('camelCase flag enables button', () => {
+    mockFlags({ devTestFlag: true })
+    const { getByTestId } = render(<Button />)
+    expect(getByTestId('test-button')).toBeTruthy()
+  })
+
+  it('camelCase flag disables button', () => {
+    mockFlags({ devTestFlag: false })
+    const { getByText } = render(<Button />)
+    expect(getByText('button disabled')).toBeTruthy()
+  })
+
+  it('kebab-case flag enables button', () => {
+    mockFlags({ 'dev-test-flag': true })
+    const { getByTestId } = render(<Button />)
+    expect(getByTestId('test-button')).toBeTruthy()
+  })
+
+  it('kebab-case flag disables button', () => {
+    mockFlags({ 'dev-test-flag': false })
+    const { getByText } = render(<Button />)
+    expect(getByText('button disabled')).toBeTruthy()
+  })
+
+  it('snake_case flag enables button', () => {
+    mockFlags({ dev_test_flag: true })
+    const { getByTestId } = render(<Button />)
+    expect(getByTestId('test-button')).toBeTruthy()
+  })
+
+  it('snake_case flag disables button', () => {
+    mockFlags({ dev_test_flag: false })
+    const { getByText } = render(<Button />)
+    expect(getByText('button disabled')).toBeTruthy()
+  })
+})
+
+describe('LaunchDarkly client interactions', () => {
+  beforeEach(() => {
+    resetLDMocks()
+  })
+
+  it('calls ldClient.track on click', () => {
+    mockFlags({ 'dev-test-flag': true })
+    const { getByTestId } = render(<Button />)
+    fireEvent.click(getByTestId('test-button'))
+    expect(ldClientMock.track).toBeCalledWith('button-click')
+  })
+
+  it('calls ldClient.identify on click', () => {
+    mockFlags({ 'dev-test-flag': true })
+    const { getByTestId } = render(<Button />)
+    fireEvent.click(getByTestId('test-button'))
+    expect(ldClientMock.identify).toBeCalledWith({ key: 'aa0ceb' })
+  })
+})
+
+describe('resetLDMocks', () => {
+  it('clears flags set in a previous test', () => {
+    mockFlags({ devTestFlag: true })
+    resetLDMocks()
+    const { getByText } = render(<Button />)
+    expect(getByText('button disabled')).toBeTruthy()
+  })
+
+  it('clears ldClient call history', () => {
+    mockFlags({ 'dev-test-flag': true })
+    const { getByTestId } = render(<Button />)
+    fireEvent.click(getByTestId('test-button'))
+    resetLDMocks()
+    expect(ldClientMock.track).not.toBeCalled()
+    expect(ldClientMock.identify).not.toBeCalled()
+  })
+})
